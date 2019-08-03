946f99b9e4e80d98e5af96bf7f4b0b0107cf25de
Jan Beich <jbeich@FreeBSD.org>
2019-08-03 21:40:37 +0000

    alloc: prefer SHM_ANON on FreeBSD a la memfd_create
    Signed-off-by: Jan Beich <jbeich@FreeBSD.org>

--- src/xshmfence_alloc.c
+++ src/xshmfence_alloc.c
@@ -78,6 +78,10 @@ xshmfence_alloc_shm(void)
 	fd = memfd_create("xshmfence", MFD_CLOEXEC|MFD_ALLOW_SEALING);
 	if (fd < 0)
 #endif
+#ifdef SHM_ANON
+	fd = shm_open(SHM_ANON, O_RDWR|O_CLOEXEC, 0600);
+	if (fd < 0)
+#endif
 	{
 #ifdef O_TMPFILE
 		fd = open(SHMDIR, O_TMPFILE|O_RDWR|O_CLOEXEC|O_EXCL, 0666);
